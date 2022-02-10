0298f5ea05ee4e840fa7cc329da2d4113a31845a
ksu <ksenya.myasoedova@gmail.com>
2022-02-10 19:49:57 +0300

    popular title film

--- app/src/main/java/com/example/themoviedb/MovieAdapter.java
+++ app/src/main/java/com/example/themoviedb/MovieAdapter.java
@@ -5,6 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -35,7 +36,7 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.Holder> {
 	@Override
 	public void onBindViewHolder(@NonNull Holder holder, int position) {
 //		final View viewById = ((ImageView) holder.itemView.findViewById(R.id.image));
-//		((TextView)holder.itemView.findViewById(R.id.name)).setText();
+		((TextView)holder.itemView.findViewById(R.id.title)).setText(popularModel.results[position].original_title);
 		Glide
 				.with(context)
 				.load("https://image.tmdb.org/t/p/original/" + popularModel.results[position].backdrop_path)
